77a0f86f5d8a94efd85c1f622cf7085eac2c48e9
zhaohaopeng <13522293962.com>
2022-01-19 16:23:24 +0800

    轮播图修改

--- src/view/home.vue
+++ src/view/home.vue
@@ -53,9 +53,11 @@ export default {
 </script>
 
 <style lang="less" scoped>
+
 .swiper {
   width: 100%;
   height: 200px;
+  margin-top: 50px;
 }
 
 .swiper-container {
@@ -82,6 +84,8 @@ export default {
   transition: 300ms;
   background: red;
   transform: scale(0.9);
+  border-radius: 6px;
+  overflow: hidden;
   img {
     width: 100%;
     height: 100%;
