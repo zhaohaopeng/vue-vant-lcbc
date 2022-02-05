411e5807019564d35d25b9a31251ff839860c572
zhaohaopeng <13522293962@163.com>
2022-02-05 23:06:08 +0800

    代码提交

--- src/view/home.vue
+++ src/view/home.vue
@@ -1,43 +1,46 @@
 <template>
-  <div class="swiper">
-    <div class="swiper-container">
-      <div class="swiper-wrapper">
-        <div class="swiper-slide">
-          <img src="../assets/0.jpg" alt="" />
+   <div class="page">
+     <img src="../assets/home.png"  >
+       <div class="swiper" v-if="false">
+        <div class="swiper-container">
+          <div class="swiper-wrapper">
+            <div class="swiper-slide">
+              <img src="../assets/0.jpg" alt="" />
+            </div>
+            <div class="swiper-slide">
+              <img src="../assets/1.jpg" alt="" />
+            </div>
+            <div class="swiper-slide">
+              <img src="../assets/2.jpg" alt="" />
+            </div>
+            <div class="swiper-slide">
+              <img src="../assets/3.jpg" alt="" />
+            </div>
+          </div>
+          <!-- Add Pagination -->
+          <div class="swiper-pagination"></div>
         </div>
-        <div class="swiper-slide">
-          <img src="../assets/1.jpg" alt="" />
-        </div>
-        <div class="swiper-slide">
-          <img src="../assets/2.jpg" alt="" />
-        </div>
-        <div class="swiper-slide">
-          <img src="../assets/3.jpg" alt="" />
-        </div>
-      </div>
-      <!-- Add Pagination -->
-      <div class="swiper-pagination"></div>
-    </div>
   </div>
+   </div>
 </template>
 
 <script>
-import Swiper from 'swiper'
-import 'swiper/css/swiper.min.css'
-import { onMounted } from 'vue'
+import Swiper from "swiper";
+import "swiper/css/swiper.min.css";
+import { onMounted } from "vue";
 export default {
-  setup () {
+  setup() {
     const initSwiper = () => {
-      new Swiper('.swiper-container', {
+      new Swiper(".swiper-container", {
         loop: true,
         slidesPerView: 1.3,
         centeredSlides: true,
         pagination: {
-          el: '.swiper-pagination',
-          clickable: true,
+          el: ".swiper-pagination",
+          clickable: true
         },
         on: {
-          click: function (e) {
+          click: function(e) {
             if (!e || !e.target) return;
             console.log(e.target, ":v");
           }
@@ -47,13 +50,19 @@ export default {
 
     onMounted(() => {
       initSwiper();
-    })
-  },
-}
+    });
+  }
+};
 </script>
 
 <style lang="less" scoped>
-
+.page {
+  width: 100vw;
+  min-height: 100vh;
+  img{
+    width: 100%;
+  }
+}
 .swiper {
   width: 100%;
   height: 200px;
